7bf371c509988f7283390af1d122b88cac886afd
IgorKozlovsky <i.kozlovsky2004@gmail.com>
2023-07-29 22:24:28 +0300

    optimieze code

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
-import { FormControl, Icon, Pressable } from 'native-base'
-import { MaterialIcons } from '@expo/vector-icons'
+import { FormControl } from 'native-base'
 
 import { StyledInputProps } from 'src/components/Input/types'
 import { InputTypes } from 'src/enums'
 import theme from 'src/theme'
-import { InputBase } from 'src/components/Input/styles'
 import { TextSmallBody } from 'src/components/Text'
+import { InputRenderers } from 'src/components/Input/constants'
+import { usePassword } from 'src/components/Input/usePassword'
 
 export const StyledInput = ({
   placeholder = '',
   label,
   type = InputTypes.TEXT,
 }: StyledInputProps): JSX.Element => {
-  const [show, setShow] = useState<boolean>(false)
+  const { show, toggleShow } = usePassword()
+
+  const inputProps = {
+    variant: 'underlined',
+    isRequired: true,
+    placeholder,
+    focusOutlineColor: theme.colors.highlight,
+  }
+
+  const renderInput = InputRenderers[type]
 
   return (
     <FormControl
@@ -22,36 +30,7 @@ export const StyledInput = ({
       }}
     >
       <TextSmallBody fontWeight="bold">{label}</TextSmallBody>
-      {type === InputTypes.PASSWORD ? (
-        <InputBase
-          type={show ? InputTypes.TEXT : InputTypes.PASSWORD}
-          variant="underlined"
-          isRequired
-          focusOutlineColor={theme.colors.highlight}
-          InputRightElement={
-            <Pressable onPress={() => setShow(!show)}>
-              <Icon
-                as={
-                  <MaterialIcons
-                    name={show ? 'visibility' : 'visibility-off'}
-                  />
-                }
-                size={6}
-                mr="2"
-                color={theme.colors.highlight}
-              />
-            </Pressable>
-          }
-          placeholder={placeholder}
-        />
-      ) : (
-        <InputBase
-          variant="underlined"
-          isRequired
-          placeholder={placeholder}
-          focusOutlineColor={theme.colors.highlight}
-        />
-      )}
+      {renderInput(inputProps, show, toggleShow)}
     </FormControl>
   )
 }
--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -5,3 +5,16 @@ export type StyledInputProps = {
   placeholder?: string
   type?: InputTypes
 }
+
+export type InputRendererProps = {
+  variant: string
+  isRequired: boolean
+  placeholder: string
+  focusOutlineColor: string
+}
+
+export type InputRenderer = (
+  props: InputRendererProps,
+  show: boolean,
+  toggleShow: () => void,
+) => JSX.Element
