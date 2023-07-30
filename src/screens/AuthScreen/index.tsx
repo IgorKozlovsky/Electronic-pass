7552f11c9a0a18bc1e6891f2d4684114fc1535a1
IgorKozlovsky <i.kozlovsky2004@gmail.com>
2023-07-30 14:29:22 +0300

    button

--- src/screens/AuthScreen/index.tsx
+++ src/screens/AuthScreen/index.tsx
@@ -1,34 +1,57 @@
-import { Checkbox } from 'native-base'
+import { Button, Checkbox, Flex } from 'native-base'
+import { useNavigation } from '@react-navigation/native'
 
 import { StyledSafeAreaView } from 'src/styles'
 import { InputFlex, TextFlex } from 'src/screens/AuthScreen/styles'
 import { TextBody, TextH4 } from 'src/components/Text'
 import { StyledInput } from 'src/components/Input'
-import { InputTypes } from 'src/enums'
+import { InputTypes, Screens } from 'src/enums'
+import theme from 'src/theme'
+import { NavigationType } from 'src/types'
 
 const AuthScreen = (): JSX.Element => {
+  const navigation = useNavigation<NavigationType>()
+
   return (
     <StyledSafeAreaView
       style={{
-        gap: 36,
+        justifyContent: 'space-between',
       }}
     >
-      <TextFlex>
-        <TextH4>Увійти 🔐</TextH4>
-        <TextBody>
-          Введіть логін і пароль. Якщо ви забули їх, то запитайте у свого
-          методиста.
-        </TextBody>
-      </TextFlex>
-      <InputFlex>
-        <StyledInput placeholder="Логін" label="Логін" type={InputTypes.TEXT} />
-        <StyledInput
-          placeholder="Пароль"
-          label="Пароль"
-          type={InputTypes.PASSWORD}
-        />
-        <Checkbox value="remember">Запам'ятай мене</Checkbox>
-      </InputFlex>
+      <Flex
+        style={{
+          gap: 36,
+        }}
+      >
+        <TextFlex>
+          <TextH4>Увійти 🔐</TextH4>
+          <TextBody>
+            Введіть логін і пароль. Якщо ви забули їх, то запитайте у свого
+            методиста.
+          </TextBody>
+        </TextFlex>
+        <InputFlex>
+          <StyledInput
+            placeholder="Логін"
+            label="Логін"
+            type={InputTypes.TEXT}
+          />
+          <StyledInput
+            placeholder="Пароль"
+            label="Пароль"
+            type={InputTypes.PASSWORD}
+          />
+          <Checkbox value="remember">Запам'ятай мене</Checkbox>
+        </InputFlex>
+      </Flex>
+      <Button
+        mb={theme.spaces.lg}
+        spinnerPlacement="end"
+        isLoadingText="Надсилається"
+        onPress={() => navigation.navigate(Screens.HOME)}
+      >
+        Надіслати
+      </Button>
     </StyledSafeAreaView>
   )
 }
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,21 @@
-import { Text } from 'native-base'
+import { useNavigation } from '@react-navigation/native'
+import { Button } from 'native-base'
+
+import { Screens } from 'src/enums'
+import { NavigationType } from 'src/types'
 
 const HomeScreen = (): JSX.Element => {
-  return <Text>HomeScreen</Text>
+  const navigation = useNavigation<NavigationType>()
+
+  return (
+    <Button
+      spinnerPlacement="end"
+      isLoadingText="Надсилається"
+      onPress={() => navigation.navigate(Screens.AUTH)}
+    >
+      Надіслати
+    </Button>
+  )
 }
 
 export default HomeScreen
