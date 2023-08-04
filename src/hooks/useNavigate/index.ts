99002aa11aa8c50c40c9bfbb08df575557614d11
HotsykVictor <vitia2372087@gmail.com>
2023-08-04 15:31:25 +0300

    refactored

--- src/hooks/useNavigate/index.ts
+++ src/hooks/useNavigate/index.ts
@@ -1,14 +1,12 @@
 import { useNavigation } from '@react-navigation/native'
 import { Screens } from 'src/enums'
 import { NavigationType } from 'src/types'
-import { NavigateType } from 'src/hooks/useNavigate/types'
 
-export const useNavigate = (): NavigateType => {
+export const useNavigate = () => {
   const navigation = useNavigation<NavigationType>()
 
-  const toScreen = (screenName: Screens) => {
+  const toScreen = (screenName: Screens): void =>
     navigation.navigate(screenName)
-  }
 
   return {
     ...navigation,
--- src/hooks/useNavigate/types.ts
+++
@@ -1,6 +0,0 @@
-import { NavigationType } from 'src/types'
-import { Screens } from 'src/enums'
-
-export type NavigateType = NavigationType & {
-  toScreen: (screenName: Screens) => void
-}
